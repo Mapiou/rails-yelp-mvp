fc31745ffd8051653ef40b352901de7907398ad9
Marion Lambert <marionlambert85@googlemail.com>
2018-11-21 17:18:02 +0100

    Yelp MVP done for all tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
+  CATEGORY_LIST = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
   validates :category,  presence: true,
-                        inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+                        inclusion: { in: Restaurant::CATEGORY_LIST }
 end
